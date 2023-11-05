d6064dddc749dfc99ecdd930a72e0a975c7b10dc
zacharysilver <zachary.silver@gmail.com>
2023-11-05 15:24:13 -0600

    first try

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { SignIn } from "./app/screens/SignIn";
+import SignIn from "./app/screens/SignIn";
 export default function App() {
   return (
     <View style={styles.container}>
--- app/screens/SignIn.tsx
+++ app/screens/SignIn.tsx
@@ -1,78 +1,66 @@
-import React, { useEffect, useState } from 'react';
-import { Text, Alert, Button, View, StyleSheet, Keyboard, TouchableWithoutFeedback } from 'react-native';
-import { TextInput } from 'react-native-paper';
+import React from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
-export const SignIn =()=> {
+const SignIn = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Login</Text>
+      <Text style={{ fontSize: 16, marginTop: 10 }}>
+        Please sign in to continue
+      </Text>
+      <TouchableOpacity style={{ flexDirection: 'row', marginTop: 20 }}>
+        <Text style={{ fontSize: 16 }}>Don't have an account ?</Text>
+        <Text style={{ fontSize: 16, fontWeight: 'bold', marginLeft: 5 }}>
+          Sign up
+        </Text>
+      </TouchableOpacity>
+      <Text style={{ marginTop: 20 }}>Email</Text>
+      <TextInput
+        style={{
+          width: 300,
+          height: 40,
+          borderWidth: 1,
+          borderColor: 'gray',
+          marginBottom: 10,
+        }}
+      />
+      <Text>Password</Text>
+      <TextInput
+        style={{
+          width: 300,
+          height: 40,
+          borderWidth: 1,
+          borderColor: 'gray',
+          marginBottom: 10,
+        }}
+      />
+      <Text style={{ marginTop: 10, color: 'blue' }}>Forgot Password?</Text>
+      <TouchableOpacity
+        style={{
+          width: 300,
+          height: 40,
+          backgroundColor: 'blue',
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderRadius: 5,
+          marginTop: 20,
+        }}
+      >
+        <Text style={{ color: 'white' }}>Log in</Text>
+      </TouchableOpacity>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          marginTop: 20,
+        }}
+      >
+        <Text>Remember Me</Text>
+        {/* You can add a checkbox or toggle switch here */}
+      </View>
+      {/* Vector and logo components */}
+    </View>
+  );
+};
 
-    
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const onLogin = ()=>{
-        alert(username)
-    }
-    return (
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-
-        <View style={styles.container}>
-            <Text style = >Email</Text>
-          <TextInput
-            value={username}
-            onChangeText={(newUsername) => setUsername(newUsername)}
-            style={styles.input}
-          />
-          <TextInput
-            value={password}
-            onChangeText={(newPassword) => setPassword(newPassword)}
-            secureTextEntry={true}
-            style={styles.input}
-          />
-          <View style = {styles.button}>
-          <Button
-            title={'Login'}
-            onPress={()=>onLogin()}          
-            />
-            </View>
-        </View>
-        </TouchableWithoutFeedback>
-      );
-  }
-
-
-
-  
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#FFF',
-  },
-  button:{
-    backgroundColor:'orange',
-    fontSize: 100,
-    width: 100,
-    color: 'green',
-  },
-  input: {
-    width: 200,
-    height: 44,
-    padding: 10,
-    fontSize: 25,
-    borderWidth: 0,
-    borderColor: 'black',
-    marginBottom: 10,
-    backgroundColor: '#EEE'
-  },
-  inputext: {
-    width: 200,
-    height: 44,
-    padding: 10,
-    textAlign:'center',
-    fontWeight:'bold',
-    borderWidth: 1,
-    borderColor: 'black',
-    marginBottom: 10,
-  },
-});
+export default SignIn;
