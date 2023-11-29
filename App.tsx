94ed2ad2b67ccdcc8da6f60527de73db9b24a620
Yifei Fang <yifeifang@Yifeis-MacBook-Air.local>
2023-11-28 18:32:16 -0600

    added profile page

--- App.tsx
+++ App.tsx
@@ -1,21 +1,30 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import SignIn from "./app/screens/SignIn";
-import SignUp from "./app/screens/SignUp"
+import SignUp from "./app/screens/SignUp";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Profile from "./app/screens/Profile";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="SignIn">
-        <Stack.Screen name="SignIn" component={SignIn} options={{headerShown: false}} />
-        <Stack.Screen name="SignUp" component={SignUp} options={{headerShown: false}} />
+      <Stack.Navigator initialRouteName="Profile">
+        <Stack.Screen
+          name="SignIn"
+          component={SignIn}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Profile" component={Profile} />
       </Stack.Navigator>
     </NavigationContainer>
-
   );
 }
 
---
+++ app/screens/Profile.tsx
@@ -0,0 +1,8 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+
+const Profile = () => {
+  return <View></View>;
+};
+
+export default Profile;
