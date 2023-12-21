08fbe212164b261360726f6063c943a7808e9306
Franklin Hu <116783339+franklinhu88@users.noreply.github.com>
2023-12-20 23:35:25 -0600

    finished new post screen look (#5)
    * finished new post screen look
    
    * fix: main untouch
    
    ---------
    
    Co-authored-by: Gabriel Dong <gabrielhdong@gmail.com>

--- app/screens/Launch.tsx
+++ app/screens/Launch.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import { StyleSheet, View, Image } from "react-native";
 import logo from "../../assets/logo-with-title.png";
 
-export default function Launch() {
+import { NavigationProp, Router } from '@react-navigation/native';
+
+interface RouterProps {
+    navigation: NavigationProp<any,any>;
+}
+
+export default function Launch({ navigation }: RouterProps) {
     return (
         <View style={styles.container}>
             <Image source={logo} alt={"launch screen logo"}style={{ marginLeft: 20 }}/>
