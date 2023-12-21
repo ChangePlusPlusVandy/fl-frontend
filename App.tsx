f908efe9c0975b4b2ab9702861e102ebc4fb7210
yifeifang11 <112604508+yifeifang11@users.noreply.github.com>
2023-12-20 23:24:00 -0600

    Bottom Tab Navigation (#4)
    * react navigation tab bar created
    
    * styled navbar
    
    * added staff navbar
    
    * import fix after merge conflict
    
    ---------
    
    Co-authored-by: Yifei Fang <yifeifang@Yifeis-MacBook-Air.local>
    Co-authored-by: Gabriel Dong <gabrielhdong@gmail.com>

--- App.tsx
+++ App.tsx
@@ -1,39 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import SignIn from "./app/screens/SignIn";
 import SignUp from "./app/screens/SignUp";
-import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Profile from "./app/screens/Profile";
 import EditProfile from "./app/screens/EditProfile";
 
 const Stack = createNativeStackNavigator();
+import { NavigationContainer } from "@react-navigation/native";
+import NavBarFamily from "./app/components/NavBarFamily";
+import NavBarStaff from "./app/components/NavBarStaff";
 
 export default function App() {
+  const [user, setUser] = useState("family");
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Profile">
-        <Stack.Screen
-          name="SignIn"
-          component={SignIn}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="SignUp"
-          component={SignUp}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Profile"
-          component={Profile}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="EditProfile"
-          component={EditProfile}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
+      {user === "family" ? <NavBarFamily /> : <NavBarStaff />}
     </NavigationContainer>
   );
 }
