7724dfb6e1ed270dc07dbc3907aef8b02fac46c2
Yifei Fang <yifeifang@Yifeis-MacBook-Air.local>
2024-01-05 09:17:32 -0600

    added safe area view

--- app/screens/Attendance.tsx
+++ app/screens/Attendance.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 
 const Attendance = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>Attendance</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
