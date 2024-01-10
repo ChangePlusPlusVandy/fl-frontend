c9eef1b92276ba5c9b914a47304a32f361d9b6cc
Gabriel Dong <gabrielhdong@gmail.com>
2024-01-10 13:30:28 -0600

    small fix

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import SignIn from "./app/screens/SignIn";
 import SignUp from "./app/screens/SignUp";
