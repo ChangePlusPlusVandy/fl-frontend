cd93b5e53260476b52cc5ddedd5d2f4c67316ee3
Ryan McCauley <ryanmcly@gmail.com>
2024-01-21 23:29:38 -0500

    create auth store

---
+++ app/stores/auth.ts
@@ -0,0 +1,55 @@
+import { firebase, FirebaseAuthTypes } from "@react-native-firebase/auth";
+import { create } from "zustand";
+
+export interface CreateUserProps {
+    username: string
+    emailAddress: string
+    password: string
+    confirmPassword: string
+    type: 'Default' | 'Staff' | 'Family'
+}
+
+interface AuthStore {
+    user: FirebaseAuthTypes.User | null;
+    createAccount: ({ username, emailAddress, password, type }: CreateUserProps) => Promise<void>;
+    signIn: ({ emailAddress, password }: { emailAddress: string, password: string }) => Promise<void>;
+    logout: () => Promise<void>;
+}
+
+const useAuthStore = create<AuthStore>((set) => ({
+    user: null,
+    createAccount: async ({ username, emailAddress, password, type }: CreateUserProps) => {
+        try {
+            const { user } = await firebase.auth().createUserWithEmailAndPassword(emailAddress, password);
+            await fetch('api_url', {
+                method: 'POST',
+                body: JSON.stringify({
+                    firbaseUserId: user.uid,
+                    name: username,
+                    type: type,
+                }),
+            });
+            set({ user });
+        } catch (e) {
+            console.log(e);
+        }
+    },
+    signIn: async ({ emailAddress, password }) => {
+        try {
+            const { user } = await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
+            set({ user });
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    logout: async () => {
+        try {
+            await firebase.auth().signOut();
+            set({ user: null });
+        } catch (error) {
+            console.log(error);
+        }
+    },
+}));
+
+export default useAuthStore;
