379eb3202bd1a7be1ca4142a1c8a7ec548ee9554
Ryan McCauley <ryanmcly@gmail.com>
2024-01-23 16:32:11 -0600

    remove old rn-firebase

--- app/firebase/index.ts
+++ app/firebase/index.ts
@@ -2,14 +2,12 @@ import Config from "react-native-config";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: 'api-key',
-    authDomain: 'project-id.firebaseapp.com',
-    databaseURL: 'https://project-id.firebaseio.com',
-    projectId: 'project-id',
-    storageBucket: 'project-id.appspot.com',
-    messagingSenderId: 'sender-id',
-    appId: 'app-id',
-    measurementId: 'G-measurement-id',
+    apiKey: "AIzaSyBxOCPWJVuUEq-phN_mf54Zph7JZJLM-8E",
+    authDomain: "friends-life-1cd60.firebaseapp.com",
+    projectId: "friends-life-1cd60",
+    storageBucket: "friends-life-1cd60.appspot.com",
+    messagingSenderId: "842743778727",
+    appId: "1:842743778727:web:a4360e42602a535f093a82"
 };
 
 export const firebase = initializeApp(firebaseConfig);
