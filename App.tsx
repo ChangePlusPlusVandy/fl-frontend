022929d9116c16ed47f51afdadf85aee335e4ad3
zacharysilver <zachary.silver@gmail.com>
2024-01-30 17:51:56 -0600

    app updated

---
+++ App.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import SignIn from "./app/screens/SignIn";
+import SignUp from "./app/screens/SignUp";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Profile from "./app/screens/Profile";
+import EditProfile from "./app/screens/EditProfile";
+import NewPost from "./app/screens/NewPost";
+import Launch from "./app/screens/Launch";
+import UserHome from "./app/screens/UserHome";
+
+const Stack = createNativeStackNavigator();
+import { NavigationContainer } from "@react-navigation/native";
+import NavBarFamily from "./app/components/NavBarFamily";
+import NavBarStaff from "./app/components/NavBarStaff";
+
+export default function App() {
+  const [user, setUser] = useState("family");
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SignIn">
+        <Stack.Screen
+          name="Launch"
+          component={Launch}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignIn"
+          component={SignIn}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Profile"
+          component={Profile}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="EditProfile"
+          component={EditProfile}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="NewPost"
+          component={NewPost}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="UserTabs"
+          component={NavBarFamily}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="StaffTabs"
+          component={NavBarStaff}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
