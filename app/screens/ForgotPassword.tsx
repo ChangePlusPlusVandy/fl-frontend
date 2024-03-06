f4efce5ca96fecc4581bf345028958b9b5c58299
22rohanr <22rohanr@students.harker.org>
2024-03-05 20:16:36 -0600

    made some progress

---
+++ app/screens/ForgotPassword.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { View, TouchableOpacity, Text } from "react-native";
+
+const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default ForgotPassword;
